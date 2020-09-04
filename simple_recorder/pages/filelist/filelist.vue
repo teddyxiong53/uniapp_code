64bdfcd30dc7da5fa1e568ad478a999790bb3fbc
teddyxiong53 <1073167306@qq.com>
2020-09-04 14:51:04 +0800

    增加文件列表页面。

---
+++ simple_recorder/pages/filelist/filelist.vue
@@ -0,0 +1,36 @@
+<template>
+	<view >
+		<view class="uni-list" v-for="(filename, index) in files" :key="index">
+			<view class="uni-list-cell">
+				{{filename}}
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				files: []
+			}
+		},
+		onLoad:function(){
+			//获取文件，填入到files里面。
+			var self = this;
+			uni.getSavedFileList({
+				success:function(res){
+					//self.data.files.push( res.fileList.filePath);
+					console.log(res);
+					for(let item of res.fileList) {
+						self.files.push(item.filePath);
+					}
+				}
+			})
+		}
+	}
+	
+</script>
+
+<style>
+</style>
--- simple_recorder/pages/index/index.vue
+++ simple_recorder/pages/index/index.vue
@@ -8,6 +8,7 @@
 		<button type="default" @click="startRecord">开始录音</button>
 		<button type="default" @click="stopRecord">停止录音</button>
 		<button type="default" @click="playFile">播放录音</button>
+		<button type="default" @click="showFiles">查看文件</button>
 	</view>
 	
 </template>
@@ -31,9 +32,19 @@
 			let self = this;
 			recorderManager.onStop(function(res) {
 				console.log("recorder stop :" + JSON.stringify(res));
-				self.voicePath = res.tempFilePath;
 				self.recordTime = 0;
 				clearInterval(self.recordTimer);
+				//这里保存文件
+				//saveFile 会把临时文件移动，因此调用成功后传入的 tempFilePath 将不可用。
+				//先弹窗让用户选择路径
+				//弹窗比较难，先不做。保存默认路径吧。
+				uni.saveFile({
+					tempFilePath: res.tempFilePath,
+					success:function(res){
+						self.voicePath = res.savedFilePath;
+						console.log("save file to:" + res.savedFilePath);
+					}
+				})
 			});
 		},
 		methods:{
@@ -42,13 +53,21 @@
 				recorderManager.start();
 				let self = this;
 				this.recordTimer = setInterval(function() {
-					console.log("1s timer ");
+					//console.log("1s timer ");
 					self.recordTime++;
 				}, 1000);
+				uni.showToast({
+					complete:function(){
+						console.log("toast complete");
+					},
+					title:"开始录音",
+					duration:1000
+				})
 			},
 			stopRecord: function() {
 				console.log("stopRecord");
 				recorderManager.stop();
+				
 			},
 			playFile: function() {
 				console.log("playFile");
@@ -90,6 +109,24 @@
 						self.playTime++;
 					}, 1000);
 				}
+			},
+			showFiles:function() {
+				/*
+				uni.getSavedFileList({
+					complete: function(res) {
+						console.log(res);
+					},
+					fail: function(res) {
+						console.log(res);
+					},
+					success:function(res){
+						console.log(res.fileList);
+					}
+				})
+				*/
+			    uni.navigateTo({
+			    	url:"../filelist/filelist"
+			    })
 			}
 		}
 	}
